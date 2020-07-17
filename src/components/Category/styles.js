dcc300291256d5e0b224715e9a8135aa882f5535
unknown <jeffynho32@gmail.com>
2020-07-17 18:07:27 -0300

    add: styles - estilizando componente das categorias de setores de serviço

---
+++ src/components/Category/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+`;
+
+export const BoxList = styled.View`
+flex-direction: row;
+`;
+
+export const BoxIcon = styled.TouchableOpacity`
+  flex: 1;
+  background: #FFF;
+  margin: 2px;
+  padding: 20px;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Image = styled.Image``;
+
+export const TextIcon = styled.Text`
+  font-size: 10px;
+  color: #999999;
+
+  margin-top: 10px;
+`;
