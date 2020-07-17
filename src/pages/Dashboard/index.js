37388165649f8ec5cad67e77302a30fc9fbca766
unknown <jeffynho32@gmail.com>
2020-07-17 15:21:15 -0300

    add: Definindo estilos para tela Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import { Text } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-import { Container } from './styles';
+import logo from '../../../assets/LOGO.png';
+
+import {
+  Container,
+  Block,
+  Content,
+  Header,
+  Logo,
+  BlockLabel,
+  Salutation,
+  Label,
+  BlockSearch,
+  SearchInput,
+  High,
+  Divider,
+} from './styles';
 
 const Dashboard = () => {
   return (
     <Container>
-      <Text>Dashboard</Text>
+      <Block>
+      <Header>
+        <High>
+          <Logo source={logo} />
+          <BlockLabel>
+            <Salutation>Olá, Jefferson Couto</Salutation>
+            <Label>Bem-vindo!</Label>
+          </BlockLabel>
+        </High>
+      </Header>
+
+      <BlockSearch>
+        <MaterialIcons name="search" size={32} color="#ccc" />
+        <Divider />
+        <SearchInput
+          placeholder="O que você está procurando"
+          autoCapitalize="none"
+          returnKeyType="send"
+          onSubmitEditing={() => {}}
+        />
+      </BlockSearch>
+      </Block>
+      
+      <Content>
+        
+      </Content>
     </Container>
   );
 }
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,8 +1,74 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.KeyboardAvoidingView.attrs({
+  enabled: Platform.OS === 'ios',
+  behavior: 'padding',
+})`
   flex: 1;
-
-  justify-content: center;
   align-items: center;
 `;
+
+export const Block = styled.View`
+  width: 100%;
+  height: 200px;
+
+  background: #9F0D8B;
+
+  padding: 0 15px;
+`;
+export const Header = styled.View`
+  margin-top: 50px;
+  margin-left: -10px;
+`;
+
+export const High = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+export const Logo = styled.Image`
+  width: 100px;
+  height: 62px;
+`;
+export const BlockLabel = styled.View`
+  margin-left: 5px;
+`;
+export const Salutation = styled.Text`
+  font-size: 14px;
+  color: #fff;
+`;
+export const Label = styled.Text`
+  font-size: 30px;
+  font-weight: bold;
+  color: #fff;
+  margin-top: -5px;
+`;
+export const BlockSearch = styled.View`
+  flex-direction: row;
+  background: #fff;
+  width: 100%;
+  height: 46px;
+  border-radius: 20px;
+
+  align-items: center;
+
+  padding: 0 10px;
+  margin-top: 20px;
+
+`;
+
+export const Divider = styled.View`
+  height: 30px;
+  width: 2px;
+  background: #ddd;
+  margin: 0 10px;
+
+`;
+export const SearchInput = styled.TextInput`
+  align-self: stretch;
+  width: 85%;
+  
+`;
+
+export const Content = styled.View``;
+
+
