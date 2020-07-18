c6a871316f81fd3658c0f2a0528199a2f3b55be7
unknown <jeffynho32@gmail.com>
2020-07-17 23:52:35 -0300

    add: BottomTab - Page Category

--- src/pages/Category/index.js
+++ src/pages/Category/index.js
@@ -1,13 +1,79 @@
 import React from 'react';
-import { Text } from 'react-native';
 
-import { Container } from './styles';
+import barbeiro from '../../../assets/icons/BARBEIRO.png';
+import restaurante from '../../../assets/icons/RESTAURANTES.png';
+import pizzaria from '../../../assets/icons/PIZZARIA.png';
+import hamburgueria from '../../../assets/icons/HAMBURGUERIA.png';
+import salao from '../../../assets/icons/SALÃO.png';
+import anuncio from '../../../assets/icons/ANÚNCIOS.png';
+import confeitaria from '../../../assets/icons/CONFEITARIA.png';
+import esfirraria from '../../../assets/icons/ESFIHARIA.png';
+import personal from '../../../assets/icons/PERSONAL.png';
+
+
+
+import Background from '../../components/Background';
+
+import { Container, Label, BoxList, BoxIcon, Image, TextIcon } from './styles';
 
 const Category = () => {
   return (
-    <Container>
-      <Text>Category</Text>
-    </Container>
+    <Background>
+      <Container>
+        <Label>CATEGORIAS</Label>
+
+        <BoxList>
+          <BoxIcon>
+            <Image source={hamburgueria} />
+            <TextIcon>HAMBURGUERIA</TextIcon>
+          </BoxIcon>
+
+          <BoxIcon>
+            <Image source={pizzaria} />
+            <TextIcon>PIZZARIA</TextIcon>
+          </BoxIcon>
+
+          <BoxIcon>
+            <Image source={restaurante} />
+            <TextIcon>RESTAURANTE</TextIcon>
+          </BoxIcon>
+      </ BoxList>
+
+      <BoxList>
+        <BoxIcon>
+          <Image source={barbeiro} />
+          <TextIcon>BARBEIRO</TextIcon>
+        </BoxIcon>
+
+        <BoxIcon>
+          <Image source={salao} />
+          <TextIcon>SALÃO DE BELEZA</TextIcon>
+        </BoxIcon>
+
+        <BoxIcon>
+          <Image source={personal} />
+          <TextIcon>PERSONAL TRAINER</TextIcon>
+        </BoxIcon>
+      </BoxList>
+
+      <BoxList>
+        <BoxIcon>
+          <Image source={anuncio} />
+          <TextIcon>ANÚNCIOS</TextIcon>
+        </BoxIcon>
+
+        <BoxIcon>
+          <Image source={esfirraria} />
+          <TextIcon>ESFIRRARIA</TextIcon>
+        </BoxIcon>
+
+        <BoxIcon>
+          <Image source={confeitaria} />
+          <TextIcon>CONFEITARIA</TextIcon>
+        </BoxIcon>
+      </BoxList>
+      </Container>
+    </Background>
   );
 }
 
--- src/pages/Category/styles.js
+++ src/pages/Category/styles.js
@@ -2,7 +2,41 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  margin-top: 50px;
+  padding: 3px;
+`;
+
+export const Label = styled.Text`
+  margin-bottom: 15px;
+  margin-left: 10px;
+  font-size: 20px;
+  color: #fff;
+  font-weight: bold;
+`;
+
+export const BoxList = styled.View`
+flex-direction: row;
+`;
+
+export const BoxIcon = styled.TouchableOpacity`
+  flex: 1;
+  background: #FFF;
+  margin: 2px;
+  padding: 20px;
 
   justify-content: center;
   align-items: center;
+`;
+
+export const Image = styled.Image`
+  width: 50px;
+  height: 50px;
+`;
+
+export const TextIcon = styled.Text`
+  font-size: 8.5px;
+  font-weight: bold;
+  color: #999999;
+
+  margin-top: 10px;
 `;
