d39ec0a00df6bcff02d90421edf09df431c6d951
unknown <jeffynho32@gmail.com>
2020-07-19 13:36:23 -0300

    add: components/Address - Definindo componente que exibe os endereços do usuário

---
+++ src/components/Address/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+export const Title = styled.Text`
+  color: #eee;
+  font-weight: bold;
+  font-size: 14px;
+`; 
+export const Block = styled.View`
+  margin-top: 10px;
+`; 
+export const LabelBlock = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 3px;
+`; 
+export const Label = styled.Text`
+  font-size: 12px;
+  color: #ddd;
+  font-weight: bold;
+`; 
+export const Value = styled.Text`
+  margin-left: 5px;
+  font-size: 10px;
+  color: #ddd;
+`;
