3d7af1f57edbfac38e55a9a5cbf826f2928f4a6e
unknown <jeffynho32@gmail.com>
2020-08-03 22:20:53 -0300

    feat(page/category): Alimentando página de categorias com dados da api

--- src/pages/Category/index.js
+++ src/pages/Category/index.js
@@ -2,24 +2,13 @@ import React, { useState, useEffect } from 'react';
 
 import api from '../../services/api';
 
-import barbeiro from '../../../assets/icons/BARBEIRO.png';
-import restaurante from '../../../assets/icons/RESTAURANTES.png';
-import pizzaria from '../../../assets/icons/PIZZARIA.png';
-import hamburgueria from '../../../assets/icons/HAMBURGUERIA.png';
-import salao from '../../../assets/icons/SALÃO.png';
-import anuncio from '../../../assets/icons/ANÚNCIOS.png';
-import confeitaria from '../../../assets/icons/CONFEITARIA.png';
-import esfirraria from '../../../assets/icons/ESFIHARIA.png';
-import personal from '../../../assets/icons/PERSONAL.png';
-
-
-
 import Background from '../../components/Background';
 import Categories from '../../components/Category';
+import Loading from '../../components/Loading';
 
 import { Container, Label, List } from './styles';
 
-const Category = () => {
+const Category = ({ navigation }) => {
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(false);
 
@@ -43,17 +32,21 @@ const Category = () => {
 
   return (
     <Background>
-      <Container>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Container>
         <Label>CATEGORIAS</Label>
 
         <List
           data={categories}
           keyExtractor={(item) => String(item.id)}
           renderItem={({ item }) => (
-            <Categories category={item} />
+            <Categories category={item} navigation={navigation} />
           )}
         />
       </Container>
+      )}
     </Background>
   );
 }
