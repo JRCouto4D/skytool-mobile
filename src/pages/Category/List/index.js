9bba3b774bab0b8a5e9d349e60cfcc4e7fcfcb9d
unknown <jeffynho32@gmail.com>
2020-08-05 12:02:29 -0300

    refactor(pages/Category/List): Definindo prop-types

--- src/pages/Category/List/index.js
+++ src/pages/Category/List/index.js
@@ -1,28 +1,30 @@
+/* eslint-disable no-nested-ternary */
 import React, { useState, useEffect } from 'react';
-import api from '../../../services/api';
 import Lottie from 'lottie-react-native';
+import PropTypes from 'prop-types';
 
 import { MaterialIcons } from '@expo/vector-icons';
+import api from '../../../services/api';
 
 import ListProvider from '../../../components/ListProvider';
 import Loading from '../../../components/Loading';
 import notFound from '../../../../assets/not_found.json';
 
-import { 
-  Container, 
-  Header, 
-  BoxLeft, 
-  Label, 
-  Result, 
-  FilterButton, 
-  TextButton, 
+import {
+  Container,
+  Header,
+  BoxLeft,
+  Label,
+  Result,
+  FilterButton,
+  TextButton,
   ProviderList,
   Content,
-  Message, 
+  Message,
 } from './styles';
 
 const List = ({ route, navigation }) => {
-  const category = route.params.category;
+  const { category } = route.params;
   const [providers, setProviders] = useState([]);
   const [loading, setLoading] = useState(false);
 
@@ -34,24 +36,22 @@ const List = ({ route, navigation }) => {
         params: {
           page: 1,
           name: '',
-        }
+        },
       });
-      
+
       setProviders(response.data.providers);
       setLoading(false);
     }
 
     loadProvider();
-    
   }, []);
 
   return (
     <Container>
       {loading ? (
         <Loading />
-      ) : (
-        providers.length >= 1 ? (
-          <>
+      ) : providers.length >= 1 ? (
+        <>
           <Header>
             <BoxLeft>
               <Label>TODOS</Label>
@@ -69,28 +69,32 @@ const List = ({ route, navigation }) => {
             keyExtractor={(item) => String(item.id)}
             renderItem={({ item }) => (
               <ListProvider
-                provider={item} 
-                category={category.name} 
-                navigation={navigation} 
+                provider={item}
+                category={category.name}
+                navigation={navigation}
               />
             )}
           />
         </>
-        ) : (
-          <Content>
-            <Lottie
-              source={notFound} 
-              autoPlay 
-              autoSize 
-              loop 
-              resizeMode="contain"
-            />
-            <Message>Ainda não temos prestadores para esta categorias</Message>
-          </Content>
-        )
+      ) : (
+        <Content>
+          <Lottie
+            source={notFound}
+            autoPlay
+            autoSize
+            loop
+            resizeMode="contain"
+          />
+          <Message>Ainda não temos prestadores para esta categorias</Message>
+        </Content>
       )}
     </Container>
   );
-}
+};
+
+List.propTypes = {
+  route: PropTypes.shape(),
+  navigation: PropTypes.shape(),
+};
 
 export default List;
