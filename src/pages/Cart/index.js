4d9d94e5a2f6c044f121f780c3848d93fa1e9579
unknown <jeffynho32@gmail.com>
2020-08-11 18:50:00 -0300

    feat(pages/Cart): definindo tela que renderizará o carrinho da aplicaçao

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const Cart = () => {
+  return <Text>CARRINHO</Text>;
+}
+
+export default Cart;
