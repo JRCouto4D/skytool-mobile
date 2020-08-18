efa2cf167b781ebf27ef1a4f89019b648cf93e71
unknown <jeffynho32@gmail.com>
2020-08-18 19:45:43 -0300

    feat(pages/Cart): Definindo componente que vai renderizar os endereços do usuário

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,10 +1,116 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState, useEffect, useMemo } from 'react';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import Address from '../../components/Address2';
+import ItensCart from '../../components/ItensCart';
+import api from '../../services/api';
+
+import { 
+  Container,
+  Content,
+  BoxLoading,
+  BoxAddress,
+  Label,
+  ListAddress,
+  BoxItens,
+  Provider,
+  ListItens,
+  AddButtom,
+  TextButtom,
+  LabelBlock,
+  TagLabel,
+  CompletedButtom,
+  TextCompletedButtom,
+} from './styles';
 
 const Cart = () => {
-  return <Text>CARRINHO</Text>;
+  const { sale_id, item: itemCart, loading } = useSelector((state) => state.bag);
+  const { product } = itemCart;
+  const { profile } = useSelector((state) => state.user);
+  const [adresses, setAdresses] = useState([]);
+  const [selectedAddress, setSelectedAddress] = useState(null);
+  const [sale, setSale] = useState([]);
+  const [infoDelivery, setInfoDelivery] = useState([]);
+  const [bug, setBug] = useState(false);
+  const [total, setTotal] = useState(null);
+
+  useEffect(() => {
+    async function loadAdresses() {
+      console.tron.log(itemCart);
+      const response = await api.get('adresses', {
+        where: {
+          user_id: profile.id,
+        }
+      });
+      const data = response.data.map((address) => ({
+        ...address,
+        able: false,
+      }));
+      data[0].able = true;
+      setSelectedAddress(data[0].id);
+  
+      setAdresses(data);
+    }
+
+    loadAdresses();
+  }, []);
+
+  async function handleActiveAddress(address_id) {
+    const stateAdresses = adresses;
+    const indexAddress = stateAdresses.findIndex((state) => state.id === address_id);
+    if (stateAdresses.length === 2) {
+      if (indexAddress === 0) {
+        stateAdresses[indexAddress].able = true;
+        stateAdresses[1].able = false;
+      } else {
+        stateAdresses[indexAddress].able = true;
+        stateAdresses[0].able = false;
+      }
+    }
+    if (stateAdresses.length === 1) {
+      stateAdresses[0].able = true;
+    }
+    
+    setAdresses(stateAdresses);
+    setSelectedAddress(stateAdresses[indexAddress].id);
+    setBug(!bug);
+    
+  }
+
+  const handleListAddress = useMemo(() => {
+    return (
+      <ListAddress
+        data={adresses}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => (
+          <Address 
+            address={item}
+            handleActiveAddress={() => handleActiveAddress(item.id)}
+          />
+        )}
+      />
+    );
+  }, [adresses, bug]);
+
+  return (
+    <Container>
+      <Content>
+        <BoxAddress>
+          <Label>Entregar em:</Label>
+          {handleListAddress}
+        </BoxAddress>
+
+        <BoxItens>
+        <Provider>The Best Esfihas</Provider>
+        </BoxItens>
+        
+      </Content>
+
+      <CompletedButtom>
+        <TextCompletedButtom>Concluir pedido</TextCompletedButtom>
+      </CompletedButtom>
+    </Container>
+  );
 }
 
 export default Cart;
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,5 +1,89 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-  
+export const Container = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    flexGrow: 1,
+    flexDirection: 'column', 
+    justifyContent: 'space-between',
+  }
+})`
+  padding-bottom: 10px;
+`;
+
+export const Content = styled.View`
+  padding: 15px 0 15px 15px;
+`;
+
+export const BoxLoading = styled.View`
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  margin-bottom: 50px;
+`;
+
+export const BoxAddress = styled.View``;
+
+export const Label = styled.Text`
+  font-size: 12px;
+  font-weight: bold;
+  color: #999;
+`;
+
+export const ListAddress = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+  numColumns: 2,
+})``;
+
+export const BoxItens = styled.View`
+  background: #FFF;
+  margin-right: 15px;
+  margin-top: 20px;
+  padding: 10px;
+  border-radius: 4px;
+`;
+export const Provider = styled.Text`
+  color: #666;
+  font-weight: bold;
+  font-size: 14px;
+  padding-bottom: 10px;
+`;
+export const ListItens = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})``;
+
+export const AddButtom = styled.TouchableOpacity`
+  height: 45px;
+  border-radius: 4px;
+  background: #9F0D8B;
+
+  margin: 20px 0;
+  justify-content: center;
+  align-items: center;
+`;
+export const TextButtom = styled.Text`
+  color: #FFF;
+  font-size: 16px;
+  font-weight: bold;
+`;
+export const LabelBlock = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const TagLabel = styled.Text`
+  color: ${props => props.color};
+  font-size: ${props => `${props.size}px`};
+`;
+
+export const CompletedButtom = styled.TouchableOpacity`
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+
+  background: #00CC33;
+`;
+export const TextCompletedButtom = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  color: #FFF;
 `;
