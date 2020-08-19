ddeaec64950b85dc6b693a59ca15a4f537a43fc9
unknown <jeffynho32@gmail.com>
2020-08-19 14:59:04 -0300

    feat(pages/Payment): Definindo tela de pagamento

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, Alert } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { removeToItemCart } from '../../store/module/bag/actions';
+import { completeSaleSuccess } from '../../store/module/sale/actions';
+import { formatPrice } from '../../util/format';
 
 import Address from '../../components/Address2';
 import ItensCart from '../../components/ItensCart';
@@ -30,13 +32,47 @@ const Cart = ({ navigation }) => {
   const dispatch = useDispatch();
   const { sale_id, item: itemCart, loading } = useSelector((state) => state.bag);
   const { profile } = useSelector((state) => state.user);
-
   const [adresses, setAdresses] = useState([]);
   const [selectedAddress, setSelectedAddress] = useState(null);
   const [sale, setSale] = useState([]);
   const [infoDelivery, setInfoDelivery] = useState([]);
   const [bug, setBug] = useState(false);
   const [total, setTotal] = useState(null);
+
+  async function loadInfoDelivery() {
+    if (itemCart.length >= 1) {
+      if (sale.length >= 1) {
+        const response = await api.get(`infoDelivery/${sale[0].provider_id}`);
+  
+        setInfoDelivery(response.data);
+      }
+    } else {
+      setInfoDelivery([]);
+    }
+  }
+
+  function calcTotal() {
+    if (itemCart.length >= 1) {
+      if (sale.length >= 1 && infoDelivery.length >= 1) {
+        const total = sale[0].total + infoDelivery[0].price;
+        setTotal(total);
+      }
+    } else {
+      setTotal(null);
+    }
+  }
+
+  async function loadSale() {
+    if (sale_id) {
+      const response = await api.get('sales', {
+        where: {
+          id: sale_id,
+        }
+      });
+
+      setSale(response.data);
+    }
+  }
 
   useEffect(() => {
     async function loadAdresses() {
@@ -95,23 +131,53 @@ const Cart = ({ navigation }) => {
     );
   }, [adresses, bug]);
 
-  function removeItemToCart(item_id) {
-    dispatch(removeToItemCart(item_id));
+  async function removeItemToCart(item_id) {
+    try {
+      await api.delete(`removeItem/${item_id}`);
+      dispatch(removeToItemCart(item_id));
+    } catch (err) {
+      Alert.alert('Erro', 'Algo deu errado ao remover o item do carrinho');
+    }
   }
 
-  const listItensCartMemo = useMemo(() => (
-    <ListItens
-      data={itemCart}
-      keyExtractor={(item) => String(item.id)}
-      renderItem={({ item }) => (
-        <ItensCart 
-          item={item}
-          removeItemToCart={() => removeItemToCart(item.id)}
-          navigation={navigation} 
-        />
-      )}
-    />
-  ), [itemCart]);
+  const listItensCartMemo = useMemo(() => {
+    return (
+      <ListItens
+        data={itemCart}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => (
+          <ItensCart 
+            item={item}
+            removeItemToCart={() => removeItemToCart(item.id)}
+            navigation={navigation} 
+          />
+        )}
+      />
+    );
+  }, [itemCart]);
+
+  useMemo(() => loadSale(), [itemCart]);
+  useMemo(() => loadInfoDelivery(), [sale]);
+  useMemo(() => calcTotal(), [sale, infoDelivery]);
+
+  async function completeOrder() {
+    console.tron.log('completeOrder');
+      if (itemCart.length >= 1) {
+        if (selectedAddress) {
+          if (total) {
+            const data = {
+              total,
+              address_id: selectedAddress,
+            }
+            dispatch(completeSaleSuccess(data));
+          } 
+        } else {
+          Alert.alert('### Ops!!! ###', 'Você não pode concluir o pedido sem antes selecionar o endereço para entrega');
+        }
+      } else {
+        Alert.alert('### Ops!!! ###', 'Você não pode concluir o pedido estando com o carrinho vazio.');
+      }
+  }
 
   return (
     <Container>
@@ -142,17 +208,17 @@ const Cart = ({ navigation }) => {
 
               <LabelBlock>
                 <TagLabel color="#ccc" size={12}>Subtotal</TagLabel>
-                <TagLabel 
-                  color="#ccc" 
-                  size={12}>R$0,00
+                <TagLabel color="#ccc" size={12}>
+                  {sale.length >= 1 ? formatPrice(sale[0].total) : 'R$0,00'}
                 </TagLabel>
               </LabelBlock>
 
               <LabelBlock>
                 <TagLabel color="#aaa" size={12}>Entrega</TagLabel>
-                <TagLabel 
-                  color="#aaa" 
-                  size={12}>R$0,00
+                <TagLabel color="#aaa" size={12}>
+                  {infoDelivery.length >= 1 
+                  ? formatPrice(infoDelivery[0].price) 
+                  : 'R$0,00'}
                 </TagLabel>
               </LabelBlock>
 
@@ -160,9 +226,8 @@ const Cart = ({ navigation }) => {
                 marginTop: 20,
               }}>
                 <TagLabel color="#333" size={18}>Total</TagLabel>
-                <TagLabel 
-                  color="#333" 
-                  size={18}>R$0,00
+                <TagLabel color="#333" size={18}>
+                  {total ? formatPrice(total) : 'R$0,00'}
                 </TagLabel>
               </LabelBlock>
             </>
@@ -170,8 +235,8 @@ const Cart = ({ navigation }) => {
         </BoxItens>
       </Content>
 
-      <CompletedButtom>
-        <TextCompletedButtom>Concluir pedido</TextCompletedButtom>
+      <CompletedButtom onPress={completeOrder}>
+        <TextCompletedButtom>Definir Pagamento</TextCompletedButtom>
       </CompletedButtom>
     </Container>
   );
