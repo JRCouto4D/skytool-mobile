296716efe7a7a8710f0a0f85b73e67752d8ec0b9
unknown <jeffynho32@gmail.com>
2020-08-20 13:27:12 -0300

    feat(pages/Payment): Definindo estilos para tela de pagamento

--- src/pages/Payment/index.js
+++ src/pages/Payment/index.js
@@ -1,13 +1,92 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { useSelector } from 'react-redux';
 
-import { Container } from './styles';
+import { formatPrice } from '../../util/format';
+import Background from '../../components/Background';
+import card from '../../../assets/cards.png';
+
+import {
+  Container,
+  Content,
+  Header,
+  LabelTotal,
+  Total,
+  Label,
+  BorderSelected,
+  BoxOption,
+  Option,
+  Brand,
+  OptionLabel,
+  Block,
+  ChangeLabel,
+  BoxChangeOption,
+  BoxLeft,
+  ResponseLabel,
+  ChangeInput,
+  ImageCard,
+  ButtonPayment,
+  TextButton,
+} from './styles';
 
 const Payment = () => {
+  const { dataSale: sale } = useSelector((state) => state.sale);
   return (
-    <Container>
-      <Text>Payment</Text>
-    </Container>
+    <Background>
+      <Container>
+        <Content>
+          <Header>
+            <LabelTotal>Valor à pagar:</LabelTotal>
+            <Total>{formatPrice(sale.total)}</Total>
+          </Header>
+
+          <Label>Pagar com:</Label>
+
+          <BorderSelected visibled={true}>
+            <BoxOption>
+              <Option>
+                <Brand visibled={true} />
+              </Option>
+              <OptionLabel>DINHEIRO</OptionLabel>
+            </BoxOption>
+
+            <Block>
+              <ChangeLabel>Troco ?</ChangeLabel>
+              <BoxChangeOption>
+                <BoxLeft>
+                  <Option>
+                    <Brand visibled={true} />
+                  </Option>
+                  <ResponseLabel>SIM</ResponseLabel>
+                </BoxLeft>
+                <Option>
+                  <Brand visibled={false} />
+                </Option>
+                <ResponseLabel>NÃO</ResponseLabel>
+              </BoxChangeOption>
+              <ChangeLabel>Troco para quanto ?</ChangeLabel>
+              <ChangeInput />
+            </Block> 
+          </BorderSelected>
+
+          <BorderSelected visibled={true}>
+            <BoxOption>
+              <Option>
+                <Brand visibled={true} />
+              </Option>
+              <OptionLabel>CARTÃO</OptionLabel>
+            </BoxOption>
+
+            <Block>
+              <ImageCard source={card} />
+            </Block>
+          </BorderSelected>
+        </Content>
+
+        <ButtonPayment>
+          <TextButton>Confirmar pagamento</TextButton>
+        </ButtonPayment>
+      </Container>
+    </Background>
   );
 }
 
