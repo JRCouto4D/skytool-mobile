7053eb507fdce82dd6cf8a4fca1f2844113a0fbf
unknown <jeffynho32@gmail.com>
2020-08-21 01:55:29 -0300

    feat(pages/Confirmation): Definindo tela de confirmation da venda

---
+++ src/pages/Confirmation/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { Container } from './styles';
+
+const Confirmation = () => {
+  return (
+    <Container>
+      <Text>Confirmation</Text>
+    </Container>
+  );
+}
+
+export default Confirmation;
