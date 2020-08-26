85cc00e0e2b28b691d7fc0f6dbd7fcef1a89b2fb
unknown <jeffynho32@gmail.com>
2020-08-26 01:02:04 -0300

    feat(pages/Orders): Definindo tela que vai listar todos os pedidos de prestação de serviço feitos pelo usuário

---
+++ src/pages/Orders/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const Orders = () => {
+  return (
+    <Container>
+      <Text>Orders</Text>
+    </Container>
+  );
+};
+
+export default Orders;
