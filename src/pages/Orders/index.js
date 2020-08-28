569fd8c6e139ed6f8e2e037a3c9f56a9d83248f5
unknown <jeffynho32@gmail.com>
2020-08-27 21:10:43 -0300

    feat(pages/Orders): Definindo estilos da tela de pedidos

--- src/pages/Orders/index.js
+++ src/pages/Orders/index.js
@@ -1,12 +1,66 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
 
-import { Container } from './styles';
+import Deliveries from '../../components/Deliveries';
+import Schedules from '../../components/Schedules';
+import {
+  Container,
+  Header,
+  Content,
+  Block,
+  DeliveryButton,
+  TextDeliveryButton,
+  SchedulesButton,
+  TextSchedulesButton,
+  LineFooter,
+  BoxButton,
+} from './styles';
 
 const Orders = () => {
+  const [viewDelivery, setViewDelivery] = useState(true);
+  const [viewSchedules, setViewSchedules] = useState(false);
+
+  function handleViewDelivery() {
+    setViewDelivery(true);
+    setViewSchedules(false);
+  }
+
+  function handleViewSchedules() {
+    setViewSchedules(true);
+    setViewDelivery(false);
+  }
+
   return (
     <Container>
-      <Text>Orders</Text>
+      <Header>
+        <Block selected={viewDelivery}>
+          <DeliveryButton onPress={handleViewDelivery}>
+            <BoxButton>
+              <MaterialIcons name="store" size={20} color={viewDelivery ? '#9F0D8B' : '#eee'} />
+              <TextDeliveryButton selected={viewDelivery}>Delivery</TextDeliveryButton>
+            </BoxButton>
+          </DeliveryButton>
+          <LineFooter selected={viewDelivery} />
+        </Block>
+
+        <Block selected={viewSchedules}>
+          <SchedulesButton onPress={handleViewSchedules}>
+            <BoxButton>
+              <MaterialIcons name="event" size={20} color={viewSchedules ? '#9F0D8B' : '#eee'} />
+              <TextSchedulesButton selected={viewSchedules}>Agendamentos</TextSchedulesButton>
+            </BoxButton>
+          </SchedulesButton>
+          <LineFooter selected={viewSchedules} />
+        </Block>
+      </Header>
+
+      <Content>
+        {viewDelivery ? (
+          <Deliveries />
+        ) : (
+          <Schedules />
+        )}
+      </Content>
     </Container>
   );
 };
--- src/pages/Orders/styles.js
+++ src/pages/Orders/styles.js
@@ -1,8 +1,62 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
+  background: #eee;
+`;
 
+export const Header = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  background: #9F0D8B;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+`;
+
+export const Block = styled.View`
+  margin-top: 30px;
+  align-items: center;
+  width: 50%;
+  background: ${props => props.selected ? '#eee' : '#9F0D8B'};
+`;
+
+export const BoxButton = styled.View`
+  flex-direction: row;
   justify-content: center;
   align-items: center;
+  margin-bottom: 10px;
+`;
+export const DeliveryButton = styled(RectButton)`
+  padding-top: 20px;
+  width: 100%;
+`;
+export const TextDeliveryButton = styled.Text`
+  margin-left: 10px;
+  color: ${props => props.selected ? '#9F0D8B' : '#eee'};
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const SchedulesButton = styled(RectButton)`
+  padding-top: 20px;
+  width: 100%;
+`;
+export const TextSchedulesButton = styled.Text`
+  margin-left: 10px;
+  color: ${props => props.selected ? '#9F0D8B' : '#eee'};
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const LineFooter = styled.View`
+  width: 70%;
+  height: 2px;
+  margin-top: -2px;
+
+  background: ${props => props.selected ? '#9F0D8B' : '#eee'};
+  display: ${props => props.selected ? 'flex' : 'none'}
 `;
