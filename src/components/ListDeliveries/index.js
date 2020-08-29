fe41fe774dc634f842ef7fcd431950a107eb3262
unknown <jeffynho32@gmail.com>
2020-08-28 22:54:15 -0300

    feat(components/ListDeliveries): Correções no estilo do componente

--- src/components/ListDeliveries/index.js
+++ src/components/ListDeliveries/index.js
@@ -1,16 +1,155 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { MaterialIcons } from '@expo/vector-icons';
 
-import MoreDetails from './MoreDetails';
-import LessDetails from './LessDetails';
-
+import Loading from '../Loading';
+import load from '../../../assets/loading.json';
 import { styles } from '../../util/globalStyles';
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Header,
+  BoxRight,
+  Logo,
+  BoxInfo,
+  InfoProvider,
+  ProviderName,
+  ProviderSector,
+  InfoOrder,
+  BlockLabel,
+  Label,
+  Value,
+  MoreStatusButton,
+  Body,
+  BlockStatus,
+  BoxStatus,
+  BoxBall,
+  Ball,
+  Status,
+  LineStatus,
+  BlockOrder,
+  BoxOrder,
+  Footer,
+  DetailsButton,
+  TextDetailsButton,
+  LessStatusButton,
+} from './styles';
 
 
-const ListDeliveries = () => {
+const ListDeliveries = ({ order, handleDetailstrue, handleDetailsfalse }) => {
+  const { order: item, details } = order;
+  console.tron.log(item);
+
   return (
     <Container style={styles.containerStyle}>
-      <MoreDetails />
+      <Content>
+        <Header>
+          <Logo source={{ uri: item.provider.avatar.url}} />
+          <BoxRight>
+            <BoxInfo>
+              <InfoProvider>
+                <ProviderName>{item.provider.name}</ProviderName>
+                <ProviderSector>{item.provider.category.name}</ProviderSector>
+              </InfoProvider>
+
+              <InfoOrder>
+                <BlockLabel>
+                  <Label>Pedido:</Label>
+                  <Value>{`#${item.id}`}</Value>
+                </BlockLabel>
+
+                <BlockLabel>
+                  <Label>Data:</Label>
+                  <Value>26/08/2020</Value>
+                </BlockLabel>
+
+                <BlockLabel>
+                  <Label>Status:</Label>
+                  <Value>Saiu para entrega</Value>
+                </BlockLabel>
+              </InfoOrder>
+            </BoxInfo>
+          </BoxRight>
+          {!details.view && (
+            <MoreStatusButton onPress={handleDetailstrue}>
+              <MaterialIcons name="keyboard-arrow-down" size={25} color="#ccc" />
+            </MoreStatusButton>
+          )}
+        </Header>
+
+        {details.view && (
+          <>
+            <Body>
+              <BlockStatus>
+                <LineStatus />
+                <BoxStatus>
+                  <BoxBall>
+                    <Ball status={true} />
+                  </BoxBall>
+                  <Status>Aceito</Status>
+                </BoxStatus>
+
+                <BoxStatus>
+                  <BoxBall>
+                    <Ball status={true} />
+                  </BoxBall>
+                  <Status>Em produção</Status>
+                </BoxStatus>
+
+                <BoxStatus>
+                  <BoxBall>
+                    <Ball status={false} />
+                  </BoxBall>
+                  <Status>Saiu para entrega</Status>
+                </BoxStatus>
+
+                <BoxStatus>
+                  <BoxBall>
+                    <Ball status={false} />
+                  </BoxBall>
+                  <Status>Entregue</Status>
+                </BoxStatus>
+              </BlockStatus>
+
+              <BlockOrder>
+                <BoxOrder>
+                  <BlockLabel>
+                    <Label>Aceito às:</Label>
+                    <Value>20:06 hs</Value>
+                  </BlockLabel>
+
+                  <BlockLabel>
+                    <Label>Saiu para entrega às:</Label>
+                    <Value>20:32 hs</Value>
+                  </BlockLabel>
+                </BoxOrder>
+
+                <BoxOrder>
+                  <BlockLabel>
+                    <Label>Produção às:</Label>
+                    <Value>20:18 hs</Value>
+                  </BlockLabel>
+
+                  <BlockLabel>
+                    <Label>Entregue às:</Label>
+                    <Value>20:52 hs</Value>
+                  </BlockLabel>
+                </BoxOrder>
+              </BlockOrder>
+            </Body>
+
+            <Footer>
+              <DetailsButton>
+                <TextDetailsButton>+ Detalhes</TextDetailsButton>
+              </DetailsButton>
+
+              <LessStatusButton onPress={handleDetailsfalse}>
+                <MaterialIcons name="keyboard-arrow-up" size={25} color="#ccc" />
+              </LessStatusButton>
+            </Footer>
+          </>
+        )}
+      </Content>
     </Container>
   );
 }
--- src/components/ListDeliveries/styles.js
+++ src/components/ListDeliveries/styles.js
@@ -3,3 +3,150 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   width: 100%;
 `;
+
+export const Content = styled.View`
+  width: 100%;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const BoxRight = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  max-width: 75%;
+`;
+
+export const Logo = styled.Image`
+  width: 80px;
+  height: 80px;
+  border-radius: 40px;
+`;
+
+export const BoxInfo = styled.View`
+  margin-left: 8px;
+`;
+
+export const InfoProvider = styled.View``;
+
+export const ProviderName = styled.Text`
+  font-size: 12px;
+  font-weight: bold;
+  color: #333;
+`;
+
+export const ProviderSector = styled.Text`
+  font-size: 6px;
+  font-weight: bold;
+  color: #ccc;
+  margin-top: -1px;
+`;
+
+export const InfoOrder = styled.View`
+  margin-top: 3px;
+`;
+
+export const BlockLabel = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Label = styled.Text`
+  font-size: 8px;
+  font-weight: bold;
+  color: #333;
+  margin-right: 5px;
+`;
+
+export const Value = styled.Text`
+  font-size: 8px;
+  color: #333;
+`;
+
+export const MoreStatusButton = styled.TouchableOpacity`
+  border: 1px solid #eee;
+  padding: 2px;
+  border-radius: 4px;
+
+  position: absolute;
+  bottom: 0;
+  right: 0;
+`;
+
+export const Body = styled.View`
+  margin-top: 15px;
+`;
+export const BlockStatus = styled.View`
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 0 10px;
+`;
+export const BoxStatus = styled.View`
+  width: 25%;
+  align-items: center;
+  justify-content: center;
+`;
+export const BoxBall = styled.View`
+  width: 10px;
+  height: 10px;
+  border-radius: 5px;
+  background: #9F0D8B;
+
+  justify-content: center;
+  align-items: center;
+`;
+export const Ball = styled.View`
+  width: 7px;
+  height: 7px;
+  border-radius: 3.5px;
+  background: ${props => props.status ? '#9F0D8B' : '#FFF'};
+`;
+export const Status = styled.Text`
+  margin-top: 3px;
+  font-size: 6px;
+  font-weight: bold;
+  color: #9F0D8B;
+`;
+export const LineStatus = styled.View`
+  width: 100%;
+  height: 1px;
+  background: #9F0D8B;
+
+  position: absolute;
+  top: 5px;
+`;
+
+export const BlockOrder = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 15px;
+`;
+
+export const BoxOrder = styled.View`
+`;
+
+export const Footer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 5px;
+`;
+export const DetailsButton = styled.TouchableOpacity`
+  padding: 5px 10px 5px 0;
+`;
+export const TextDetailsButton = styled.Text`
+  font-size: 10px;
+  font-weight: bold;
+  color: #9F0D8B;
+`;
+
+export const LessStatusButton = styled.TouchableOpacity`
+  border: 1px solid #eee;
+  padding: 2px;
+  border-radius: 4px;
+`;
