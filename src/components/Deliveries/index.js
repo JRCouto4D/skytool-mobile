0d5acf8dbbb7ba8c818fe847390390c1e8d5f694
unknown <jeffynho32@gmail.com>
2020-08-28 22:52:48 -0300

    feat(components/Deliveries): Alimentando componente que lista todas os pedidos

--- src/components/Deliveries/index.js
+++ src/components/Deliveries/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { MaterialIcons } from '@expo/vector-icons';
 
+import Loading from '../../components/Loading';
+import gears from '../../../assets/gears_2.json';
 import ListDeliveries from '../../components/ListDeliveries';
+import api from '../../services/api';
 
 import {
   Container,
@@ -15,7 +18,79 @@ import {
 } from './styles';
 
 const Deliveries = () => {
-  const deliveries = [1];
+  const [loading, setLoading] = useState(false);
+  const [bug, setBug] = useState(false);
+  const [orders, setOrders] = useState([]);
+
+  async function loadOrders() {
+    setLoading(true);
+
+    const response = await api.get('orders/list');
+    const data = response.data.orders.map((order) => ({
+      order: {...order},
+      details: {
+        view: false,
+      }
+    }));
+    setOrders(data);
+    setBug(!bug);
+
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    loadOrders();
+  }, []);
+
+  async function handleDetailstrue(id) {
+    const response = await api.get('orders/list');
+    const newData = response.data.orders.map(or => (
+      or.id === id 
+      ? { order: {
+        ...or,
+      }, details: {
+        view: true,
+      }} 
+      : { order: {
+        ...or,
+      }, details: {
+        view: false,
+      }}
+    ));
+    setOrders(newData);
+    setBug(!bug);
+  }
+
+  async function handleDetailsfalse(id) {
+    const newData = orders.map(or => (
+      or.order.id === id 
+      ? { order: {
+        ...or.order,
+      }, 
+      details: {
+        view: false,
+      }
+    } : or
+    ));
+    setOrders(newData);
+    setBug(!bug);
+  }
+
+  const ListMemoDeliveries = useMemo(() => {
+    return (
+      <List
+        data={orders}
+        keyExtractor={(item) => String(item.order.id)}
+        renderItem={({ item }) => (
+          <ListDeliveries
+            order={item} 
+            handleDetailstrue={() => handleDetailstrue(item.order.id)} 
+            handleDetailsfalse={() => handleDetailsfalse(item.order.id)}
+          />
+        )}
+      />
+    );
+  }, [bug]);
 
   return (
     <Container>
@@ -31,13 +106,9 @@ const Deliveries = () => {
         </FilterButton>
       </Header>
 
-      <List
-        data={deliveries}
-        keyExtractor={(item) => String(item)}
-        renderItem={() => (
-          <ListDeliveries />
-        )}
-      />
+      {loading ? (
+        <Loading animation={gears} />
+      ) : ListMemoDeliveries}
     </Container>
   );
 }
